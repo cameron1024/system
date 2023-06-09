95740347d1a6897418625de3f6627bdb7ad71646
Cameron <cameron.studdstreet@gmail.com>
2023-06-09 15:34:45 +0100

    rust-analyzer seems to work

--- modules/home/rust.nix
+++ modules/home/rust.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
+
 {
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   home.packages = with pkgs; [
     rustup
     gcc
@@ -9,6 +9,8 @@
     cmake
     glibc
     ninja
+
+    # rust-analyzer
 
     # (rust-analyzer.overrideAttrs (oldAttrs: { 
     #   RUSTC_FLAGS = "-Ctarget-cpu=native";
