40c574c99827ffc05d8df269be53c94fb7bda488
Cameron <cameron.studdstreet@gmail.com>
2024-02-09 19:12:07 +0000

    refactor home moduels

---
+++ modules/home/tools/neovim/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, isDarwin, ... }:
+let
+   
+  lldbAdapter = pkgs.vscode-extensions.vadimcn.vscode-lldb.adapter;
+
+  linuxPackages = with pkgs;[
+    neovide
+  ];
+
+  macosPackages = [];
+
+in
+{
+  home.packages = with pkgs; [
+    neovim
+
+    nil  # nix lsp
+    lua-language-server
+    # lldb
+    # lldbAdapter
+
+  ] ++ (if isDarwin then macosPackages else linuxPackages); 
+
+  home.file."./.config/nvim/" = {
+    source = ./nvim;
+    recursive = true;
+  };
+
+  # home.sessionVariables = {
+  #   LIBLLDB_PATH = "${lldbAdapter}/lib/libcodelldb.so";
+  #   CODELLDB_PATH = "${lldbAdapter}/bin/codelldb";
+  # };
+}
