1f5730491df9e8086be3db7ed983c4d4e0d79b41
cameron <cameron.studdstreet@gmail.com>
2024-11-27 04:06:20 +0000

    stuff

--- home/neovim2/nvim/lua/plugins/editing/quicker.lua
+++ home/neovim2/nvim/lua/plugins/editing/quicker.lua
@@ -2,9 +2,24 @@ return {
   "stevearc/quicker.nvim",
   lazy = true,
   event = "FileType qf",
-  opts = {},
   keys = {
     { "<C-q>", function() require 'quicker'.toggle() end },
     { "<C-tab>", function() require 'quicker'.toggle {loclist = true} end },
-  }
+  },
+  opts = {
+    keys = {
+      {
+        ">",
+        function ()
+          require 'quicker'.expand { before = 2, after = 2, add_to_existing = true }
+        end,
+      },
+      {
+        "<",
+        function ()
+          require 'quicker'.collapse()
+        end,
+      }
+    }
+  },
 }
