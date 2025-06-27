07380aa67eccb89eb83a804f78a2ed93fb9bad08
cameron <cameron.studdstreet@gmail.com>
2025-06-27 16:45:28 +0100

    simplify mkSystem a bit

--- nixos/default.nix
+++ nixos/default.nix
@@ -2,15 +2,17 @@
   mkSystem = {
     system,
     spec ? {},
-    hardware,
-    config ? {},
     homeManager ? true,
     optimizations ? true,
+    modules ? [],
+    extraOverlays ? [],
   }: let
-    overlays = import ../overlays {
-      inherit inputs optimizations;
-      arch = spec.cpuArch or null;
-    };
+    overlays =
+      (import ../overlays {
+        inherit inputs optimizations;
+        arch = spec.cpuArch or null;
+      })
+      ++ extraOverlays;
 
     machine = spec;
 
@@ -24,14 +26,13 @@
       inherit specialArgs;
 
       modules =
-        [
+        modules
+        ++ [
           ./common.nix
-          hardware
           {
             inherit machine;
             nixpkgs.overlays = overlays;
           }
-          config
         ]
         ++ (inputs.nixpkgs.lib.optionals homeManager [
           inputs.home-manager.nixosModules.default
@@ -47,74 +48,85 @@ in {
   thinkchad = mkSystem {
     system = "x86_64-linux";
     spec = import ./machines/specs/thinkpad.nix {inherit inputs;};
-    hardware = ./hardware/thinkpad.nix;
+    modules = [
+      ./hardware/thinkpad.nix
+      {
+        system.stateVersion = "24.11";
+        services'.standardMachine.enable = true;
+        gpu'.arch = "intel";
 
-    config = {
-      system.stateVersion = "24.11";
-      services'.standardMachine.enable = true;
-      gpu'.arch = "intel";
+        boot.loader.efi.efiSysMountPoint = "/boot";
 
-      boot.loader.efi.efiSysMountPoint = "/boot";
+        networking.hostName = "thinkchad";
 
-      networking.hostName = "thinkchad";
+        programs'.niri.enable = true;
+        services'.desktop.displays = with import ./machines/displays.nix; [
+          thinkpadBuiltin
+        ];
 
-      programs'.niri.enable = true;
-      services'.desktop.displays = with import ./machines/displays.nix; [
-        thinkpadBuiltin
-      ];
-
-      programs.steam.enable = true;
-    };
+        programs.steam.enable = true;
+      }
+    ];
   };
 
   mini = mkSystem {
     system = "x86_64-linux";
     spec = import ./machines/specs/mini.nix {inherit inputs;};
-    hardware = ./hardware/mini2.nix;
+    modules = [
+      ./hardware/mini2.nix
+      {
+        system.stateVersion = "24.11";
+        services'.standardMachine.enable = true;
+        gpu'.arch = "zen5";
 
-    config = {
-      system.stateVersion = "24.11";
-      services'.standardMachine.enable = true;
-      gpu'.arch = "zen5";
+        boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
-      boot.binfmt.emulatedSystems = ["aarch64-linux"];
+        networking.hostName = "mini";
 
-      networking.hostName = "mini";
+        services'.jellyfin.enable = true;
+        services'.immich.enable = true;
+        services'.home-assistant.enable = true;
 
-      services'.jellyfin.enable = true;
-      services'.immich.enable = true;
-      services'.home-assistant.enable = true;
+        programs'.niri.enable = true;
+        services'.desktop.displays = with import ./machines/displays.nix; [
+          alien
+        ];
 
-      programs'.niri.enable = true;
-      services'.desktop.displays = with import ./machines/displays.nix; [
-        alien
-      ];
-
-      programs.steam.enable = true;
-    };
+        programs.steam.enable = true;
+      }
+    ];
   };
 
   pi = mkSystem {
     system = "aarch64-linux";
-    hardware = inputs.nixos-hardware.nixosModules.raspberry-pi-4;
     homeManager = false;
-    config = {lib, ...}: {
-      system.stateVersion = "25.11";
+    extraOverlays = [
+      # rpi kernel has some missing kernel modules - build fails without this
+      (final: super: {
+        makeModulesClosure = x:
+          super.makeModulesClosure (x // {allowMissing = true;});
+      })
+    ];
+    modules = [
+      inputs.nixos-hardware.nixosModules.raspberry-pi-4
+      ({lib, ...}: {
+        system.stateVersion = "25.11";
 
-      networking.hostName = "pi";
-      networking.wireless.enable = false;
+        networking.hostName = "pi";
+        networking.wireless.enable = false;
 
-      users.users."cameron" = {
-        isNormalUser = true;
-        extraGroups = ["wheel" "networkmanager" "video"];
-        initialPassword = "password";
-        initialHashedPassword = lib.mkForce null;
-      };
+        users.users."cameron" = {
+          isNormalUser = true;
+          extraGroups = ["wheel" "networkmanager" "video"];
+          initialPassword = "password";
+          initialHashedPassword = lib.mkForce null;
+        };
 
-      users.users.root.initialHashedPassword = "";
+        users.users.root.initialHashedPassword = "";
 
-      services'.openssh.enable = true;
-      services'.adguardhome.enable = true;
-    };
+        services'.openssh.enable = true;
+        services'.adguardhome.enable = true;
+      })
+    ];
   };
 }
