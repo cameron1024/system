bca5f88e46fab6c735bffbb2df6282b816d7bbc0
cameron <cameron.studdstreet@gmail.com>
2025-08-31 03:00:34 +0100

    schemastore

--- home/neovim/lang/json.nix
+++ home/neovim/lang/json.nix
@@ -4,4 +4,15 @@
   plugins.schemastore.enable = true;
   plugins.schemastore.json.enable = true;
   plugins.schemastore.yaml.enable = true;
+
+  lsp.servers.jsonls.settings.json.schemaStore.url = "";
+  lsp.servers.jsonls.settings.json.schemas.__raw = ''
+    require "schemastore".json.schemas()
+  '';
+
+  lsp.servers.yamlls.settings.yaml.schemaStore.enable = false;
+  lsp.servers.yamlls.settings.yaml.schemaStore.url = "";
+  lsp.servers.yamlls.settings.yaml.schemas.__raw = ''
+    require "schemastore".yaml.schemas()
+  '';
 }
