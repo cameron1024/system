26c16cbbe67989085457479a4e377dabdab1c497
cameron <cameron.studdstreet@gmail.com>
2026-03-22 06:23:09 +0000

    stuff

--- home/neovim/lang/lean.nix
+++ home/neovim/lang/lean.nix
@@ -7,16 +7,16 @@
   };
   dependencies.lean.enable = lib.mkForce false;
 
-  plugins.treesitter.grammarPackages = [
-    (pkgs.tree-sitter.buildGrammar {
-      language = "lean";
-      version = "0.0.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "Julian";
-        repo = "tree-sitter-lean";
-        rev = "efe6b87145608d12f5996bd7f0cf6095a0e82261";
-        hash = "sha256-MF+LRzhDw3V/l/h11ZTyWCUCm3b+g0oyOdaCZMVlJc4=";
-      };
-    })
-  ];
+  # plugins.treesitter.grammarPackages = [
+  #   (pkgs.tree-sitter.buildGrammar {
+  #     language = "lean";
+  #     version = "0.0.0";
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "Julian";
+  #       repo = "tree-sitter-lean";
+  #       rev = "efe6b87145608d12f5996bd7f0cf6095a0e82261";
+  #       hash = "sha256-MF+LRzhDw3V/l/h11ZTyWCUCm3b+g0oyOdaCZMVlJc4=";
+  #     };
+  #   })
+  # ];
 }
