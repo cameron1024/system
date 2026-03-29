d14a463257331c52f439ad3cf8e5e0f804cb0be3
cameron <cameron.studdstreet@gmail.com>
2026-03-29 04:46:18 +0100

    more changes

--- home/internet/qute.nix
+++ home/internet/qute.nix
@@ -1,6 +1,33 @@
-{ pkgs, ... }:
-let
+{pkgs, ...}: let
   startpage = pkgs.writeText "startpage.html" (builtins.readFile ./startpage.html);
+  colors = {
+    bg_dim = "#1e2326";
+    bg0 = "#272e33";
+    bg1 = "#2e383c";
+    bg2 = "#374145";
+    bg3 = "#414b50";
+    bg4 = "#495156";
+    bg5 = "#4f5b58";
+    bg_visual = "#4c3743";
+    bg_red = "#493b40";
+    bg_green = "#3c4841";
+    bg_blue = "#384b55";
+    bg_yellow = "#45443c";
+    fg = "#d3c6aa";
+    red = "#e67e80";
+    orange = "#e69875";
+    yellow = "#dbbc7f";
+    green = "#a7c080";
+    aqua = "#83c092";
+    blue = "#7fbbb3";
+    purple = "#d699b6";
+    grey0 = "#7a8478";
+    grey1 = "#859289";
+    grey2 = "#9da9a0";
+    statusline1 = "#a7c080";
+    statusline2 = "#d3c6aa";
+    statusline3 = "#e67e80";
+  };
 in {
   programs.qutebrowser = {
     enable = true;
@@ -18,37 +45,12 @@ in {
       "rs" = "https://docs.rs/{}";
     };
 
-    settings = let
-      colors = {
-        bg_dim = "#1e2326";
-        bg0 = "#272e33";
-        bg1 = "#2e383c";
-        bg2 = "#374145";
-        bg3 = "#414b50";
-        bg4 = "#495156";
-        bg5 = "#4f5b58";
-        bg_visual = "#4c3743";
-        bg_red = "#493b40";
-        bg_green = "#3c4841";
-        bg_blue = "#384b55";
-        bg_yellow = "#45443c";
-        fg = "#d3c6aa";
-        red = "#e67e80";
-        orange = "#e69875";
-        yellow = "#dbbc7f";
-        green = "#a7c080";
-        aqua = "#83c092";
-        blue = "#7fbbb3";
-        purple = "#d699b6";
-        grey0 = "#7a8478";
-        grey1 = "#859289";
-        grey2 = "#9da9a0";
-        statusline1 = "#a7c080";
-        statusline2 = "#d3c6aa";
-        statusline3 = "#e67e80";
+    perDomainSettings = {
+      "https://accounts.google.com/*" = {
+        content.headers.user_agent = "Mozilla/5.0 ({os_info}; rv:130.0) Gecko/20100101 Firefox/130";
       };
-    in {
-
+    };
+    settings = {
       url.start_pages = ["file://${startpage}"];
       tabs.show = "switching";
       tabs.position = "bottom";
@@ -110,4 +112,3 @@ in {
     };
   };
 }
-
