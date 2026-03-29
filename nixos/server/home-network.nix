9d8ebc4fa979b739040b9c05724c44ee98ee6479
cameron <cameron.studdstreet@gmail.com>
2026-03-29 06:12:00 +0100

    stuff

--- nixos/server/home-network.nix
+++ nixos/server/home-network.nix
@@ -26,8 +26,6 @@
           dns.bootstrap_dns = ["1.1.1.1"];
           dns.upstream_dns = ["1.1.1.1"];
 
-          # Privacy: disable query logging and statistics to avoid persistently
-          # recording which domains devices on the network are visiting
           querylog.enabled = false;
           statistics.enabled = false;
         };
@@ -35,8 +33,6 @@
         host = "0.0.0.0";
       };
 
-      # Use local AdGuard Home for DNS resolution, bypassing the router.
-      # 1.1.1.1 is a fallback in case AdGuard Home is down.
       networking.nameservers = ["127.0.0.1" "1.1.1.1"];
 
       networking.firewall.allowedTCPPorts = [53];
@@ -48,9 +44,6 @@
         enable = true;
         httpPort = 3000;
 
-        # By default, monitors all interfaces. Once port mirroring is set up
-        # on a managed switch, change this to the specific interface receiving
-        # mirrored traffic, e.g. ["enp2s0"]
         interfaces = ["any"];
 
         extraConfig = ''
@@ -61,19 +54,6 @@
       };
 
       networking.firewall.allowedTCPPorts = [3000];
-
-      # After first boot, configure the following in the ntopng web UI
-      # (http://<host>:3000, default credentials admin/admin):
-      #
-      # Settings > Preferences > Data Retention:
-      #   - Set to 30 days
-      #
-      # Settings > Preferences > Timeseries:
-      #   - Host Timeseries: "Light" (default, records traffic volume per host)
-      #   - L7 Application Timeseries: "None" (avoids recording which apps/services each device uses)
-      #
-      # Do NOT enable:
-      #   - Flow dump to ClickHouse (this would persistently log individual connections)
     })
   ];
 }
